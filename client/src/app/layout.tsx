318880e9633c3b2737f3440a3718b6e9a87f7e01
okobizdev5-prog <okobizdev5@gmail.com>
2026-07-23 16:35:35 +0600

    feat: implement root layout with dynamic metadata, global state providers, and third-party tracking scripts

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -15,10 +15,10 @@ export async function generateMetadata(): Promise<Metadata> {
 
   const icons = logoUrl && logoUrl.startsWith("http")
     ? {
-        icon: logoUrl,
-        shortcut: logoUrl,
-        apple: logoUrl,
-      }
+      icon: logoUrl,
+      shortcut: logoUrl,
+      apple: logoUrl,
+    }
     : undefined;
 
   return {
@@ -80,6 +80,30 @@ export default async function RootLayout({
             `,
           }}
         />
+
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1519962375868494');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript><img height="1" width="1" className="hidden"
+          src="https://www.facebook.com/tr?id=1519962375868494&ev=PageView&noscript=1"
+        /></noscript>
+
+
       </head>
       <body className="antialiased" suppressHydrationWarning>
         <style
